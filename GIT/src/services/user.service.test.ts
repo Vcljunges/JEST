507963a85de2faf9eab9fed7407468c382c3c989
Vinícius <vcljunges@gmail.com>
2026-03-27 16:41:35 -0300

    Atualização do user test

--- GIT/src/services/user.service.test.ts
+++ GIT/src/services/user.service.test.ts
@@ -1,13 +1,20 @@
 import { UserService } from "./user.service.js";
 
 describe("Testing user service", () => {
-    
+    let userService: UserService
+    const name = "test"
+    const email = "[EMAIL_ADDRESS]"
+    const password = "123456789"
+    const role = "DEFAULT"
+
     beforeAll(async () => {
         userService = new UserService()
     })
 
     it("should create a new user", async () => {
-        const user = await userService.create({ name, email, password, role })
+        const newUser = await userService.create({ name, email, password, role })
+
+        expect(newUser).toHaveProperty("id")
+        expect(newUser.email).toBe(email)
     })
-    
 })
